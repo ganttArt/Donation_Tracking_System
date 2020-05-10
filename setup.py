801aa48a0c22366f9bdd457e02dc056934346436
Chris Gantt <gantt.art@gmail.com>
2020-05-10 14:42:44 -0700

    Setup file altered for heroku deployment

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ db.connect()
 
 # This line will allow you "upgrade" an existing database by
 # dropping all existing tables from it.
-db.drop_tables([Donor, Donation])
+# db.drop_tables([Donor, Donation])
 
 db.create_tables([Donor, Donation])
 
